2477d5a0514dafaa2c8422e0666f882a806e3999
Yuliia <yuliianeznakhina@gmail.com>
2026-04-21 10:09:08 +0300

    finished category card css

--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -2,8 +2,13 @@ import './CategoryCard.css';
 
 export function CategoryCard(props) {
   return <div className="CategoryCard">
-    <img src={props.img} alt={props.name}></img>
-    <p>{props.name}</p>
-    <a>See all <img src="./images/icons/card-arrow-right.png"></img></a>
+    <div id='information'>
+      <img src={props.img} alt={props.name}></img>
+      <p>{props.name}</p>
+    </div>
+    <div id='seeAll'>
+      <a>See all</a>
+      <img src="./images/icons/card-arrow-right.png"></img>
+    </div>
   </div>
 }
