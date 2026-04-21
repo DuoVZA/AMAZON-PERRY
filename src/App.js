e838767486123db2caf5a63829da51ba166c64d3
Yuliia <yuliianeznakhina@gmail.com>
2026-04-21 09:23:23 +0300

    sorted images into folders and added category card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { CategoryCard } from "./components/CategoryCard/CategoryCard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CategoryCard img="./images/categories/Tools-for-leather-products-favorable-prices.png" name="Tools for leather products: favorable prices"/>
     </div>
   );
 }
